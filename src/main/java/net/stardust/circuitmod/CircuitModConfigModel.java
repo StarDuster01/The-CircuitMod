af79d1b77cf3f6cd75a026e33dfdd7dc93b28540
greatcow <nis.miner@gmail.com>
2024-03-27 20:55:35 +0100

    Config future settings

--- src/main/java/net/stardust/circuitmod/CircuitModConfigModel.java
+++ src/main/java/net/stardust/circuitmod/CircuitModConfigModel.java
@@ -6,13 +6,12 @@ import io.wispforest.owo.config.annotation.*;
 @Modmenu(modId = "circuitmod")
 @Config(name = "circuitmod-config", wrapperName = "CircuitModConfig")
 public class CircuitModConfigModel {
+    @SectionHeader("powerSettings")
     @Nest
     public powerScalingCat powerScaling = new powerScalingCat();
-
     @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public static class powerScalingCat {
-        @RangeConstraint(min = 0.1d, max = 2.0d)
-        public double powerGenerationScale = 1.0;
+        public float powerGenerationScale = 1.0f;
         public solarGenerationMode solarMode = solarGenerationMode.NORMAL;
         public enum solarGenerationMode {
             NORMAL, REALISTIC, BUFFED;
@@ -20,8 +19,19 @@ public class CircuitModConfigModel {
     }
 
     @Nest
-    public AltNest altNested = new AltNest();
-    public static class AltNest {
-        public boolean testBool = false;
+    public windMultiplierCat windSettings = new windMultiplierCat();
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+    public static class windMultiplierCat {
+        public boolean windUseBiomes = true;
+        public float WindBiomeMultiplierStandard = 1.0f;
+        public float windBiomeMultiplierPeaks = 1.4f;
+        public float WindBiomeMultiplierHills = 1.2f;
+        public float WindBiomeMultiplierShore = 1.1f;
+        public float WindBiomeMultiplierPlains = 1.0f;
+        public float WindBiomeMultiplierForest = 0.8f;
+        public boolean windUseWeather = true;
+        public float WindWeatherMultiplierClear = 1.0f;
+        public float WindWeatherMultiplierRain = 1.3f;
+        public float WindWeatherMultiplierStorm = 1.5f;
     }
 }
